36cd19121fda7e355b0f3366a1b0bcc1914aedaa
dasha <dasha@DESKTOP-OL5785C>
2023-09-27 20:00:16 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+       
+
+        Random random = new Random();
+        int[,] array = new int[5, 5];
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i, j] = random.Next(0, 10);
+            }
+        }
+
+
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                if (i == 0 || i == array.GetLength(0) - 1 ||
+                    j == 0 || j == array.GetLength(1) - 1)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                else
+                {
+                    Console.Write("  ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        {
+            Console.WriteLine("Esli hotite povtorit vvod, vvedite 1, inache lubuyu dr cifru");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a == 1) Main();
+            return;
+        }
+        Console.ReadLine();
+
+    }
+}
